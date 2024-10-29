c8230f213af9d03bf42b4766c98a30c3d01c5ea5
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2024-10-29 20:48:30 +0100

    Refactor image component

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -1,11 +1,71 @@
-const Image = ({ src, alt, className, width = 'auto', height = 'auto' }) => {
+import { useState, Suspense } from "react";
+import errorImg from "/error.jpeg";
+import "./index.css";
 
+const LazyImage = ({
+  src,
+  srcSet,
+  alt = "",
+  className,
+  size = {
+    width: "auto",
+    height: "auto",
+    minWidth: "100%",
+    minHeight: "100%",
+  },
+  objectFit = "cover",
+  loading = "lazy",
+  errorImage = errorImg,
+  onLoad,
+  onError, // e.g. send analytics
+}) => {
+  const [isError, setIsError] = useState(false);
+  
+  // for error handling we can create and use ErrorBoundary Component(wrapper to Suspense) instead of using state based error handling
   const handleImageError = (e) => {
-    e.target.alt = 'Image failed to load';
+    e.target.alt = "Image failed to load";
+    setIsError(true);
+    onError?.(e);
   };
 
-  return <img src={src} alt={alt} className={className} width={width} height={height} onError={handleImageError} />;
+  const handleImageLoad = (e) => {
+    onLoad?.(e);
+  };
 
+  const isImageDecorative = !alt;
+
+  return (
+    <div
+      style={{
+        ...size,
+        overflow: "hidden",
+      }}
+      className={className}
+    >
+      <img
+        data-testid="image"
+        src={isError ? errorImage : src}
+        srcSet={srcSet}
+        alt={alt}
+        loading={loading}
+        aria-hidden={isImageDecorative}
+        onError={handleImageError}
+        onLoad={handleImageLoad}
+        className="img"
+        style={{
+          objectFit: objectFit,
+        }}
+      />
+    </div>
+  );
+};
+
+const Image = ({ loadingElement = <div>Loading...</div>, ...props }) => {
+  return (
+    <Suspense fallback={loadingElement}>
+      <LazyImage {...props} />
+    </Suspense>
+  );
 };
 
 export default Image;
