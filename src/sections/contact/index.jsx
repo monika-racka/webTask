03f99af4824a8c056d6b21f39d9e85078254dcda
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2024-10-29 16:57:35 +0100

    Add elements to contact section

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,12 +1,71 @@
-import './index.css';
+import { useState, useCallback } from "react";
+import Heading from "../../components/heading";
+import Text from "../../components/text";
+import FormInput from "../../components/formInput";
+import "./index.css";
+
+const defaultFormData = {
+  firstName: "",
+  lastName: "",
+  phoneNumber: "",
+  interests: "",
+};
 
 const Contact = () => {
-
   return (
-    <div className="contact">
-      :)
-    </div>
-  )
+    //id could be used for navigation
+    <section className="contactSection" id="contact">
+      <div className="container">
+        <div className="content">
+          <Heading level={2}>Contact</Heading>
+          <Text
+            size="14px"
+            className="description"
+          >
+            Questions or concerns? Just fill out the form below and our support
+            team will get back to you within 24 hours
+          </Text>
+        </div>
+        <form className="form">
+          <div className="name">
+            <FormInput
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              required={true}
+              className="input"
+            />
+            <FormInput
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              required={true}
+              className="input"
+            />
+          </div>
+          <FormInput
+            type="tel"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            required={true}
+            style="full"
+            className="input"
+          />
+          <FormInput
+            type="text"
+            name="interests"
+            placeholder="What Service are you interested in?"
+            required={true}
+            style="full"
+            className="input"
+          />
+          <button className="submit" type="submit">
+            Submit now
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
